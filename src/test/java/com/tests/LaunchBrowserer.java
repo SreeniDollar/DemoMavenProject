4f94dfa50585477748d4f3cb36eff264b081c7d8
SreeniDollar <srinivasoptimist7@gmail.com>
2018-10-28 16:36:16 +0530

    chromedriveradded

--- src/test/java/com/tests/LaunchBrowserer.java
+++ src/test/java/com/tests/LaunchBrowserer.java
@@ -1,5 +1,7 @@
 package com.tests;
 
+
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
@@ -10,9 +12,10 @@ public class LaunchBrowserer {
 	public void launchbrowser()
 	{
 		System.out.println("Welcome to Dell Inspiron 15 3000");
-		System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
-		FirefoxDriver driver=new FirefoxDriver();
-		//ChromeDriver driver = new ChromeDriver();
+		//System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		//FirefoxDriver driver=new FirefoxDriver();
+		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.amazon.in");
 	}
